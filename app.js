6087ce4ebd74cee38ff885061a449fc35f36e667
ykkim3 <ykkim3@bsgglobal.com>
2020-11-10 19:17:47 +0900

    v2 second

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const models = require("./models/index.js");
 models.sequelize.sync().then( () => {
   console.log(" DB 연결 성공!");
 }).catch(err => {
-  console.log("연결 실패");
+  console.log("연결 실패2");
   console.log(err);
 })
 
