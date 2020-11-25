06a673d43411519cb42d3ed04b339e3385cc28f4
ykkim3 <47133379+ykkim3@users.noreply.github.com>
2020-11-25 17:54:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const models = require("./models/index.js");
 models.sequelize.sync().then( () => {
   console.log(" DB 연결 성공 1");
 }).catch(err => {
-  console.log("연결 실패");
+  console.log("연결 실패1");
   console.log(err);
 })
 
